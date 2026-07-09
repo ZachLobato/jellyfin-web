2bfb449014bfbdde65a35ac065f5337183f086b8
Zach Lobato <zacharyisaiahlobato@hotmail.com>
2026-07-09 07:38:03 -0400

    feat: dev 8081 serve

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,24 @@ const { merge } = require('webpack-merge');
 
 const common = require('./webpack.common');
 
+function getDevServerPort() {
+    const rawPort = process.env.npm_config_port || process.env.PORT;
+
+    if (!rawPort) {
+        return undefined;
+    }
+
+    const port = Number(rawPort);
+
+    if (!Number.isInteger(port) || port < 1 || port > 65535) {
+        throw new Error(`Invalid dev server port: ${rawPort}`);
+    }
+
+    return port;
+}
+
+const devServerPort = getDevServerPort();
+
 module.exports = merge(common, {
     // In order for live reload to work we must use "web" as the target not "browserslist"
     target: process.env.WEBPACK_SERVE ? 'web' : 'browserslist',
@@ -19,10 +37,12 @@ module.exports = merge(common, {
     },
     devServer: {
         compress: true,
+        ...(devServerPort ? { port: devServerPort } : {}),
         client: {
             overlay: {
                 errors: true,
-                warnings: false
+                warnings: false,
+                runtimeErrors: false
             }
         }
     }
